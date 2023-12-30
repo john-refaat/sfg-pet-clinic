422fb5052bb6c4ff0f48ede1098b5b347ed9ccd5
John <john.refaat88@gmail.com>
2023-12-30 20:41:14 +0200

    Creating Pet Type Map Service. Closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author john
+ * @since 30/12/2023
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+/**
+ * @author john
+ * @since 30/12/2023
+ */
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+}
