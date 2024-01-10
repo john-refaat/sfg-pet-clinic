ba7f0eb4dd0519bca65d60179761b627d6f18b91
John <john.refaat88@gmail.com>
2024-01-11 00:19:08 +0200

    Implementing Visit Map Service. Closes #45

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * @author john
+ * @since 10/01/2024
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
